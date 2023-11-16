b61570b6e8e3572bfd0b12646c163667e6183a58
a dinosaur <dino@a-dinosaur.com>
2023-11-17 07:30:36 +1100

    filcool: remove colemak & add virtual numpad to fn, which now also temp swaps sclk led w/ numlock

--- keyboards/filcool/keymap_dino.c
+++ keyboards/filcool/keymap_dino.c
@@ -1,63 +1,73 @@
 #include "keymap_common.h"
 
-#define KEYMAPS_SIZE 3
-const uint8_t keymaps_size = KEYMAPS_SIZE;
-const uint8_t PROGMEM keymaps[KEYMAPS_SIZE][MATRIX_ROWS][MATRIX_COLS] =
+enum layer_id
 {
-  /* Layer 0: Standard ANSI layer */
-  KEYMAP_ANSI_ISO_JIS( \
+  LAYER_BASE,      // Layer 0: Standard ANSI layer
+  LAYER_FUNCTION,  // Layer 1: Function layer
+  NUM_LAYERS
+};
+
+const uint8_t keymaps_size = NUM_LAYERS;
+const uint8_t PROGMEM keymaps[NUM_LAYERS][MATRIX_ROWS][MATRIX_COLS] =
+{
+  [LAYER_BASE] = KEYMAP_ANSI_ISO_JIS( \
     ESC,      F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11, F12,    PSCR,SLCK,PAUS,                         \
     GRV, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, BSPC,   INS, HOME,PGUP,    NLCK,PSLS,PAST,PMNS, \
     TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,BSLS,   DEL, END, PGDN,    P7,  P8,  P9,  PPLS, \
-    CAPS,A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,     ENT,                       P4,  P5,  P6,        \
+    FN0, A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,     ENT,                       P4,  P5,  P6,        \
     LSFT,NUBS,Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,     RSFT,        UP,           P1,  P2,  P3,  PENT, \
-    LCTL,LGUI,LALT,               SPC,                RALT,RGUI, FN0,RCTL,   LEFT,DOWN,RGHT,    P0,  PDOT),          \
-  /* Layer 1: Colemak */
-  KEYMAP_ANSI_ISO_JIS( \
-    ESC,      F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11, F12,    PSCR,SLCK,PAUS,                         \
-    GRV, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, BSPC,   INS, HOME,PGUP,    NLCK,PSLS,PAST,PMNS, \
-    TAB, Q,   W,   F,   P,   G,   J,   L,   U,   Y,   SCLN,LBRC,RBRC,BSLS,   DEL, END, PGDN,    P7,  P8,  P9,  PPLS, \
-    CAPS,A,   R,   S,   T,   D,   H,   N,   E,   I,   O,   QUOT,     ENT,                       P4,  P5,  P6,        \
-    LSFT,NUBS,Z,   X,   C,   V,   B,   K,   M,   COMM,DOT, SLSH,     RSFT,        UP,           P1,  P2,  P3,  PENT, \
-    LCTL,LGUI,LALT,               SPC,                RALT,RGUI, FN0,RCTL,   LEFT,DOWN,RGHT,    P0,  PDOT),          \
-  /* Layer 2: Function layer */
-  KEYMAP_ANSI_ISO_JIS( \
-    BTLD,     NO  ,NO,  NO,  NO,  FN3, FN4, NO,  NO,  NO,  NO,  NO,  NO,     NO,  FN2, NO,                           \
-    NLCK,NO,  NO,  NO,  NO,  NO,  NO,  NO,  NO,  NO,  NO,  NO,  NO,  NO,     VOLD,MUTE,VOLU,    NO,  NO,  NO,  NO,   \
-    CALC,NO,  NO,  NO,  NO,  NO,  NO,  NO,  NO,  NO,  NO,  NO,  NO,  NO,     MPRV,MPLY,MNXT,    NO,  NO,  NO,  NO,   \
-    NO,  NO,  NO,  NO,  NO,  NO,  NO,  NO,  NO,  NO,  NO,  NO,       NO,                        NO,  NO,  NO,        \
-    NO,  NO,  NO,  NO,  NO,  NO,  NO,  NO,  NO,  NO,  NO,  NO,       NO,          NO,           NO,  NO,  NO,  NO,   \
+    LCTL,LGUI,LALT,               SPC,                RALT,RGUI, FN0,RCTL,   LEFT,DOWN,RGHT,    P0,  PDOT),
+  [LAYER_FUNCTION] = KEYMAP_ANSI_ISO_JIS( \
+    BTLD,     NO  ,NO,  NO,  NO,  NO,  NO,  NO,  NO,  NO,  NO,  NO,  NO,     NO,  NO,  NO,                           \
+    NO,  NO,  NO,  NO,  NO,  NO,  NLCK,PSLS,PAST,PMNS,NO,  NO,  NO,  BSPC,   VOLD,MUTE,VOLU,    NO,  NO,  NO,  NO,   \
+    CAPS,NO,  NO,  NO,  NO,  CALC,P7,  P8,  P9,  PPLS,NO,  NO,  NO,  NO,     MPRV,MPLY,MNXT,    NO,  NO,  NO,  NO,   \
+    TRNS,NO,  NO,  NO,  NO,  FN1, P4,  P5,  P6,  PENT,NO,  NO,       NO,                        NO,  NO,  NO,        \
+    NO,  NO,  NO,  NO,  NO,  NO,  P0,  P1,  P2,  P3,  PDOT,NO,       NO,          NO,           NO,  NO,  NO,  NO,   \
     NO,  NO,  NO,                 NO,                 NO,  NO,  TRNS,NO,     NO,  NO,  NO,      NO,  NO)
 };
 
 
 enum macro_id
 {
-  DOUBLE_ZERO,
-  GREGGO,
-};
-
-#define FN_ACTIONS_SIZE 5
-const uint8_t fn_actions_size = FN_ACTIONS_SIZE;
-const action_t PROGMEM fn_actions[FN_ACTIONS_SIZE] =
-{
-  [0] = ACTION_LAYER_MOMENTARY(2),
-  [1] = ACTION_MACRO(DOUBLE_ZERO),
-  [2] = ACTION_MACRO(GREGGO),
-  [3] = ACTION_DEFAULT_LAYER_SET(0),
-  [4] = ACTION_DEFAULT_LAYER_SET(1)
+  MACRO_DOUBLE_ZERO
 };
 
 const macro_t* action_get_macro(keyrecord_t* record, uint8_t id, uint8_t opt)
 {
   switch (id)
   {
-  case DOUBLE_ZERO: return record->event.pressed
+  case MACRO_DOUBLE_ZERO: return record->event.pressed
     ? MACRO(T(0), T(0), END)
     : MACRO_NONE;
-  case GREGGO: return record->event.pressed
-    ? MACRO( D(LSFT), T(SCLN), T(G), U(LSFT), T(R), T(E), T(G), T(G), T(O), D(LSFT), T(SCLN), END )
-    : MACRO_NONE;
   default: return MACRO_NONE;
+  }
+}
+
+
+enum function_id
+{
+  FN0_FUNCTION,     // FN0: Hold for function layer
+  FN1_DOUBLE_ZERO,  // FN1: Numpad double zeros
+  NUM_FUNCTIONS
+};
+
+const uint8_t fn_actions_size = NUM_FUNCTIONS;
+const action_t PROGMEM fn_actions[NUM_FUNCTIONS] =
+{
+  [FN0_FUNCTION]    = ACTION_LAYER_MOMENTARY(LAYER_FUNCTION),
+  [FN1_DOUBLE_ZERO] = ACTION_MACRO(MACRO_DOUBLE_ZERO)
+};
+
+
+extern bool swap_num_scroll;
+extern void update_leds(void);
+
+void hook_layer_change(uint32_t layer_state)
+{
+  const bool function = (1 << LAYER_FUNCTION) == layer_state;
+  if (function != swap_num_scroll)
+  {
+    swap_num_scroll = function;
+    update_leds();
   }
 }
--- keyboards/filcool/led.c
+++ keyboards/filcool/led.c
@@ -1,17 +1,31 @@
 #include <stdint.h>
+#include <stdbool.h>
 #include <avr/io.h>
 #include "led.h"
 
-void led_set(uint8_t usb_led)
+#define CAPLOCK_BIT (1 << USB_LED_CAPS_LOCK)
+#define NUMLOCK_BIT (1 << USB_LED_NUM_LOCK)
+#define SCRLOCK_BIT (1 << USB_LED_SCROLL_LOCK)
+
+static uint8_t ledstate = 0;
+bool swap_num_scroll = false;
+
+void update_leds(void)
 {
   CONFIG_LED_IO;
 
-  if (usb_led & (1 << USB_LED_CAPS_LOCK))
+  if (ledstate & CAPLOCK_BIT)
   { USB_LED_CAPS_LOCK_ON; } else { USB_LED_CAPS_LOCK_OFF; }
 
-  if (usb_led & (1 << USB_LED_NUM_LOCK))
+  if (ledstate & (swap_num_scroll ? SCRLOCK_BIT : NUMLOCK_BIT))
   { USB_LED_NUM_LOCK_ON; } else { USB_LED_NUM_LOCK_OFF; }
 
-  if (usb_led & (1 << USB_LED_SCROLL_LOCK))
+  if (ledstate & (swap_num_scroll ? NUMLOCK_BIT : SCRLOCK_BIT))
   { USB_LED_SCROLL_LOCK_ON; } else { USB_LED_SCROLL_LOCK_OFF; }
+}
+
+void led_set(uint8_t usb_led)
+{
+  ledstate = usb_led & (CAPLOCK_BIT | NUMLOCK_BIT | SCRLOCK_BIT);
+  update_leds();
 }
