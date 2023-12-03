156c5f6d90c07a13aaaad7292497c77babf65563
a dinosaur <dino@a-dinosaur.com>
2023-12-04 10:36:25 +1100

    grandpad: simplify default keymap

--- keyboards/grandpad/keymap_default.c
+++ keyboards/grandpad/keymap_default.c
@@ -8,8 +8,7 @@
 /// Layer definitions
 enum layer_id
 {
-	LAYER_BASE,      // Layer 0: Base layer
-	LAYER_FUNCTION,  // Layer 1: Function layer
+	LAYER_BASE,  // Layer 0: Base layer
 
 	NUM_LAYERS
 };
@@ -20,16 +19,9 @@ const uint8_t keymaps[NUM_LAYERS][MATRIX_ROWS][MATRIX_COLS] KEYMAP_SECTION =
 		ESC, TAB, PEQL,BSPC,
 		NLCK,PSLS,PAST,PMNS,
 		P7,  P8,  P9,  PPLS,
-		P4,  P5,  P6,  PEQL,
-		P1,  P2,  P3,  PEQL,
-		P0,  FN1, PDOT,PENT),
-	[LAYER_FUNCTION] = KEYMAP(
-		NO,  NO,  NO,  NO,
-		BTLD,NO,  NO,  NO,
-		LBRC,RBRC,NO,  NO,
-		NO,  NO,  NO,  NO,
-		NO,  NO,  NO,  NO,
-		NO,  FN2, NO,  NO  )
+		P4,  P5,  P6,  LBRC,
+		P1,  P2,  P3,  RBRC,
+		P0,  FN0, PDOT,PENT)
 };
 
 
@@ -37,8 +29,7 @@ const uint8_t keymaps[NUM_LAYERS][MATRIX_ROWS][MATRIX_COLS] KEYMAP_SECTION =
 /// Macro defintions
 enum macro_id
 {
-	MACRO_DOUBLEZERO,
-	MACRO_TRIPLEZERO
+	MACRO_DOUBLEZERO
 };
 
 const macro_t* action_get_macro(keyrecord_t* record, uint8_t id, uint8_t opt)
@@ -46,7 +37,6 @@ const macro_t* action_get_macro(keyrecord_t* record, uint8_t id, uint8_t opt)
 	switch (id)
 	{
 	case MACRO_DOUBLEZERO: return record->event.pressed ? MACRO(T(P0), T(P0), END) : MACRO_NONE;
-	case MACRO_TRIPLEZERO: return record->event.pressed ? MACRO(T(P0), T(P0), T(P0), END) : MACRO_NONE;
 	default: return MACRO_NONE;
 	}
 }
@@ -56,17 +46,12 @@ const macro_t* action_get_macro(keyrecord_t* record, uint8_t id, uint8_t opt)
 /// Function & action definitions
 enum function_id
 {
-	FN0_LAYER_FUNCTION,
-
-	FN1_MACRO_DOUBLEZERO,
-	FN2_MACRO_TRIPLEZERO,
+	FN0_MACRO_DOUBLEZERO,
 
 	NUM_FUNCTIONS
 };
 
 const action_t fn_actions[NUM_FUNCTIONS] ACTION_SECTION =
 {
-	[FN0_LAYER_FUNCTION]   = ACTION_LAYER_MOMENTARY(LAYER_FUNCTION),
-	[FN1_MACRO_DOUBLEZERO] = ACTION_MACRO(FN1_MACRO_DOUBLEZERO),
-	[FN2_MACRO_TRIPLEZERO] = ACTION_MACRO(FN2_MACRO_TRIPLEZERO)
+	[FN0_MACRO_DOUBLEZERO] = ACTION_MACRO(FN0_MACRO_DOUBLEZERO)
 };
