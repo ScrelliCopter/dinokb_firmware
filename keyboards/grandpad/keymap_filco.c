ac6f836c3fea5ccc3afa2fd0d866ffb8d4e8cf4c
a dinosaur <dino@a-dinosaur.com>
2023-12-05 11:09:20 +1100

    grandpad: unify filco macro & kp key handling

--- keyboards/grandpad/keymap_filco.c
+++ keyboards/grandpad/keymap_filco.c
@@ -27,12 +27,11 @@ const uint8_t keymaps[NUM_LAYERS][MATRIX_ROWS][MATRIX_COLS] KEYMAP_SECTION =
 };
 
 
-//////////////////
-/// Advanced logic
+////////////////
+/// Action setup
 enum action_id
 {
 	ACTION_VIRTUAL_NUMLOCK,
-	ACTION_DOUBLEZERO,
 
 	ACTION_ZERO_INSERT,
 	ACTION_ONE_END,
@@ -45,61 +44,23 @@ enum action_id
 	ACTION_EIGHT_UP,
 	ACTION_NINE_PGUP,
 	ACTION_PERIOD_DEL,
+	ACTION_DOUBLEZERO,
+	ACTION_EQUALS,
 
 	FIRST_KP_ACTION = ACTION_ZERO_INSERT,
-	LAST_KP_ACTION = ACTION_PERIOD_DEL,
+	LAST_KP_ACTION = ACTION_EQUALS,
 	KP_ACTIONS_LENGTH = LAST_KP_ACTION + 1 - FIRST_KP_ACTION
 };
 
-static volatile bool real_numlock = false, virt_numlock = true;
-static volatile uint8_t numlock_toggle_held = 0;
-static volatile uint8_t kp_pressed[KP_ACTIONS_LENGTH];
+static volatile uint8_t kp_action_down[KP_ACTIONS_LENGTH];
+
 void led_rgb_update(void);
 
 void hook_late_init(void)
 {
 	for (uint8_t i = 0; i < KP_ACTIONS_LENGTH; ++i)
-		kp_pressed[i] = KC_NO;
+		kp_action_down[i] = KC_NO;
 	led_rgb_update();
-}
-
-
-////////////////////
-/// Macro defintions
-enum macro_id
-{
-	MACRO_EQUALS,
-	MACRO_DOUBLEZERO
-};
-
-//TODO: fold this stuff into actions
-const macro_t* action_get_macro(keyrecord_t* record, uint8_t id, uint8_t opt)
-{
-	if (id == MACRO_EQUALS || id == MACRO_DOUBLEZERO)
-	{
-		if (id == MACRO_DOUBLEZERO && !virt_numlock) return MACRO_NONE;
-		if (record->event.pressed)
-		{
-			const macro_t* macro;
-			if (numlock_toggle_held || real_numlock)
-				macro = id == MACRO_EQUALS
-					? MACRO(D(LALT), T(KP_6), T(KP_1), U(LALT), END)
-					: MACRO(T(P0), T(P0), END);
-			else
-				macro = id == MACRO_EQUALS
-					? MACRO(T(NUMLOCK), D(LALT), T(KP_6), T(KP_1), U(LALT), END)
-					: MACRO(T(NUMLOCK), T(KP_0), T(KP_0), END);
-			if (!real_numlock || numlock_toggle_held)
-				++numlock_toggle_held;
-			return macro;
-		}
-		else if (numlock_toggle_held && --numlock_toggle_held == 0)
-		{
-			return MACRO(T(NUMLOCK), END);
-		}
-	}
-
-	return MACRO_NONE;
 }
 
 
@@ -137,29 +98,86 @@ const action_t PROGMEM fn_actions[NUM_FUNCTIONS] ACTION_SECTION =
 	[FN7_SEVEN_HOME]       = ACTION_FUNCTION(ACTION_SEVEN_HOME),
 	[FN8_EIGHT_UP]         = ACTION_FUNCTION(ACTION_EIGHT_UP),
 	[FN9_NINE_PGUP]        = ACTION_FUNCTION(ACTION_NINE_PGUP),
-	[FN10_DOUBLEZERO]      = ACTION_MACRO(MACRO_DOUBLEZERO),
+	[FN10_DOUBLEZERO]      = ACTION_FUNCTION(ACTION_DOUBLEZERO),
 	[FN11_PERIOD_DEL]      = ACTION_FUNCTION(ACTION_PERIOD_DEL),
 	[FN12_VIRTUAL_NUMLOCK] = ACTION_FUNCTION(ACTION_VIRTUAL_NUMLOCK),
-	[FN13_EQUALS]          = ACTION_MACRO(MACRO_EQUALS)
+	[FN13_EQUALS]          = ACTION_FUNCTION(ACTION_EQUALS)
 };
+
+static volatile bool real_numlock = false, virt_numlock = true;
+
+void keypad_function(keyrecord_t* record, uint8_t id)
+{
+	volatile static uint16_t kp_action_nlcktoggle = 0ul;
+
+	const uint8_t kpid = id - FIRST_KP_ACTION;
+	const uint16_t kpid_bit = 1ul << kpid;
+
+	if (record->event.pressed)
+	{
+		const bool use_nav = !virt_numlock && real_numlock;
+
+		uint8_t key = KC_NO;
+		const macro_t* macro = MACRO_NONE;
+		switch (id)
+		{
+		case ACTION_ZERO_INSERT: key = use_nav ? KC_INSERT : KC_KP_0; break;
+		case ACTION_ONE_END:     key = use_nav ? KC_END : KC_KP_1; break;
+		case ACTION_TWO_DOWN:    key = use_nav ? KC_DOWN : KC_KP_2; break;
+		case ACTION_THREE_PGDN:  key = use_nav ? KC_PGDOWN : KC_KP_3; break;
+		case ACTION_FOUR_LEFT:   key = use_nav ? KC_LEFT : KC_KP_4; break;
+		case ACTION_FIVE:        key = use_nav ? KC_NO : KC_KP_5; break;
+		case ACTION_SIX_RIGHT:   key = use_nav ? KC_RIGHT : KC_KP_6; break;
+		case ACTION_SEVEN_HOME:  key = use_nav ? KC_HOME : KC_KP_7; break;
+		case ACTION_EIGHT_UP:    key = use_nav ? KC_UP : KC_KP_8; break;
+		case ACTION_NINE_PGUP:   key = use_nav ? KC_PGUP : KC_KP_9; break;
+		case ACTION_PERIOD_DEL:  key = use_nav ? KC_DELETE : KC_KP_DOT; break;
+		case ACTION_DOUBLEZERO:  macro = virt_numlock ? MACRO(T(P0), T(P0), END) : MACRO_NONE; break;
+		case ACTION_EQUALS:      macro = MACRO(D(LALT), T(KP_6), T(KP_1), U(LALT), END); break;
+		default: break;
+		}
+
+		if (IS_KEY(key) || macro != MACRO_NONE)
+		{
+			if (kp_action_nlcktoggle || (!real_numlock && virt_numlock))
+			{
+				if (!kp_action_nlcktoggle)
+				{
+					register_code(KC_NUMLOCK);
+					unregister_code(KC_NUMLOCK);
+					//FIXME: do we need a delay after this?
+				}
+				kp_action_nlcktoggle |= kpid_bit;
+			}
+
+			if (IS_KEY(key))
+				register_code(kp_action_down[kpid] = key);
+			if (macro != MACRO_NONE)
+				action_macro_play(macro);
+		}
+	}
+	else
+	{
+		if (kp_action_nlcktoggle)
+		{
+			kp_action_nlcktoggle &= ~kpid_bit;
+			if (!kp_action_nlcktoggle)
+			{
+				register_code(KC_NUMLOCK);
+				unregister_code(KC_NUMLOCK);
+			}
+		}
+
+		if (IS_KEY(kp_action_down[kpid]))
+		{
+			unregister_code(kp_action_down[kpid]);
+			kp_action_down[kpid] = KC_NO;
+		}
+	}
+}
 
 void action_function(keyrecord_t* record, uint8_t id, uint8_t opt)
 {
-	static const uint8_t numpad_key_lut[KP_ACTIONS_LENGTH][2] =
-	{
-		{ KC_KP_0, KC_INSERT },
-		{ KC_KP_1, KC_END },
-		{ KC_KP_2, KC_DOWN },
-		{ KC_KP_3, KC_PGDOWN },
-		{ KC_KP_4, KC_LEFT },
-		{ KC_KP_5, KC_NO },
-		{ KC_KP_6, KC_RIGHT },
-		{ KC_KP_7, KC_HOME },
-		{ KC_KP_8, KC_UP },
-		{ KC_KP_9, KC_PGUP },
-		{ KC_KP_DOT, KC_DELETE }
-	};
-
 	if (id == ACTION_VIRTUAL_NUMLOCK)
 	{
 		if (record->event.pressed)
@@ -170,36 +188,7 @@ void action_function(keyrecord_t* record, uint8_t id, uint8_t opt)
 	}
 	else if (id >= FIRST_KP_ACTION && id <= LAST_KP_ACTION)
 	{
-		const uint8_t key_id = id - FIRST_KP_ACTION;
-		if (record->event.pressed)
-		{
-			const uint8_t pressed = numpad_key_lut[key_id][!virt_numlock && real_numlock];
-			if (IS_KEY(pressed))
-			{
-				if (numlock_toggle_held || (!real_numlock && virt_numlock))
-				{
-					if (numlock_toggle_held++ == 0)
-					{
-						register_code(KC_NUMLOCK);
-						unregister_code(KC_NUMLOCK);
-					}
-				}
-				register_code(pressed);
-				kp_pressed[key_id] = pressed;
-			}
-		}
-		else if (IS_KEY(kp_pressed[key_id]))
-		{
-			const uint8_t release_key = kp_pressed[key_id];
-			kp_pressed[key_id] = KC_NO;
-			if (numlock_toggle_held && --numlock_toggle_held == 0)
-			{
-				register_code(KC_NUMLOCK);
-				unregister_code(KC_NUMLOCK);
-			}
-			unregister_code(release_key);
-			send_keyboard_report();
-		}
+		keypad_function(record, id);
 	}
 }
 
@@ -218,4 +207,5 @@ void led_set_user(uint8_t state)
 	const bool new_state = (state & _BV(USB_LED_NUM_LOCK)) == _BV(USB_LED_NUM_LOCK);
 	if (new_state != real_numlock)
 		real_numlock = new_state;
+	//TODO: check if host toggling real numlock off should clear kp_action_nlcktoggle
 }
